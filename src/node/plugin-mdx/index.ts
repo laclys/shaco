0b8d1c0e3475f30c722c17f33b520e3aa2df0591
Lac <laclys@126.com>
2023-05-31 23:29:07 +0800

    feat: add mdx-hmr plugin

--- src/node/plugin-mdx/index.ts
+++ src/node/plugin-mdx/index.ts
@@ -1,6 +1,7 @@
 import { Plugin } from 'vite'
 import { pluginMdxRollup } from './pluginMdxRollup'
+import { pluginMdxHMR } from './plugin-mdx-hmr'
 
 export async function createPluginMdx(): Promise<Plugin[]> {
-  return [await pluginMdxRollup()]
+  return [await pluginMdxRollup(), pluginMdxHMR()]
 }
