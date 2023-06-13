48118d331b7339feb7f0d5a138a5c8954c5dfd0e
Lac <laclys@126.com>
2023-06-13 21:24:42 +0800

    feat: update unocssOpts

--- src/node/unocssOpts.ts
+++ src/node/unocssOpts.ts
@@ -7,6 +7,9 @@ const options: VitePluginConfig = {
     presetWind({}), // 兼容 Windi CSS, Tailwind CSS
     presetIcons()
   ],
+  shortcuts: {
+    'flex-center': 'flex justify-center items-center'
+  },
   rules: [
     [
       /^divider-(\w+)$/,
@@ -25,7 +28,38 @@ const options: VitePluginConfig = {
         content: '" "'
       }
     ]
-  ]
+  ],
+  theme: {
+    colors: {
+      brandLight: 'var(--shaco-c-brand-light)',
+      brandDark: 'var(--shaco-c-brand-dark)',
+      brand: 'var(--shaco-c-brand)',
+      text: {
+        1: 'var(--shaco-c-text-1)',
+        2: 'var(--shaco-c-text-2)',
+        3: 'var(--shaco-c-text-3)',
+        4: 'var(--shaco-c-text-4)'
+      },
+      divider: {
+        default: 'var(--shaco-c-divider)',
+        light: 'var(--shaco-c-divider-light)',
+        dark: 'var(--shaco-c-divider-dark)'
+      },
+      gray: {
+        light: {
+          1: 'var(--shaco-c-gray-light-1)',
+          2: 'var(--shaco-c-gray-light-2)',
+          3: 'var(--shaco-c-gray-light-3)',
+          4: 'var(--shaco-c-gray-light-4)'
+        }
+      },
+      bg: {
+        default: 'var(--shaco-c-bg)',
+        soft: 'var(--shaco-c-bg-soft)',
+        mute: 'var(--shaco-c-bg-mute)'
+      }
+    }
+  }
 }
 
 export default options
